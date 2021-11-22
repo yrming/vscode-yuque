7bd71c5cc47373b3a8c739d697d647e80543d0e6
YRM <yrm@outlook.com>
2021-11-22 11:07:22 +0800

    feat: extract function for creating and editing repository

--- src/tree/repos/reposTreeProvider.ts
+++ src/tree/repos/reposTreeProvider.ts
@@ -1,8 +1,14 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import * as vscode from 'vscode';
 import { v4 as uuidv4 } from 'uuid';
 import { YuqueClient, YuqueDoc, YuqueRepo, YuqueUserDetail } from '../../@types/type';
 import { DocsTreeItem } from '../common/docsTreeItem';
 import { ReposTreeItem } from './ReposTreeItem';
+
+enum AddOrEditEnum {
+	Add = 'Add',
+    Edit = 'Edit'
+}
 
 export class ReposTreeProvider implements vscode.TreeDataProvider<ReposTreeItem> {
     private _onDidChangeTreeData: vscode.EventEmitter<ReposTreeItem | undefined | void> = new vscode.EventEmitter<ReposTreeItem | undefined | void>();
@@ -12,41 +18,11 @@ export class ReposTreeProvider implements vscode.TreeDataProvider<ReposTreeItem>
 
     constructor(private context: vscode.ExtensionContext, private client: YuqueClient, private user: YuqueUserDetail) {
         vscode.commands.registerCommand('yuque.repos.create', async () => {
-            const name = await vscode.window.showInputBox({ placeHolder: '请输入知识库名称' });
-            if (name) {
-                const value = await vscode.window.showQuickPick(['仅自己可见（自己和知识库成员可见）', '互联网可见（互联网所有人可见）'], {
-                    placeHolder: '请选择可见范围'
-                });
-                if(value) {
-                    const publicVal = value === '互联网可见（互联网所有人可见）' ? 1 : 0;
-                    try {
-                        await this.client.repos.create({ user: this.user.id, data: { name: name, slug: uuidv4(), type: "Book", public: publicVal } });
-                        vscode.window.showInformationMessage('创建成功！');
-                        this.refresh();
-                    } catch (error) {
-                        console.log(error);
-                    }
-                }
-            }
+            this._addOrEditRepo(AddOrEditEnum.Add);
         });
 
         vscode.commands.registerCommand('yuque.repos.edit', async (treeItem: ReposTreeItem) => {
-            const name = await vscode.window.showInputBox({ value: treeItem.label, placeHolder: '请输入知识库名称' });
-            if (name) {
-                const value = await vscode.window.showQuickPick(['仅自己可见（自己和知识库成员可见）', '互联网可见（互联网所有人可见）'], {
-                    placeHolder: '请选择可见范围'
-                });
-                if(value) {
-                    const publicVal = value === '互联网可见（互联网所有人可见）' ? 1 : 0;
-                    try {
-                        await this.client.repos.create({ user: this.user.id, data: { name: name, slug: uuidv4(), type: "Book", public: publicVal } });
-                        vscode.window.showInformationMessage('修改成功！');
-                        this.refresh();
-                    } catch (error) {
-                        console.log(error);
-                    }
-                }
-            }
+            this._addOrEditRepo(AddOrEditEnum.Edit, treeItem);
         });
 
         vscode.commands.registerCommand('yuque.repos.delete', async (treeItem: ReposTreeItem) => {
@@ -68,6 +44,26 @@ export class ReposTreeProvider implements vscode.TreeDataProvider<ReposTreeItem>
         vscode.commands.registerCommand('yuque.repos.refresh', async () => {
             this.refresh();
         });
+    }
+
+    async _addOrEditRepo(type: AddOrEditEnum, treeItem?: ReposTreeItem) {
+        const msg = type === AddOrEditEnum.Add ? '创建成功！' : '修改成功！';
+        const name = await vscode.window.showInputBox({ value: treeItem?.label, placeHolder: '请输入知识库名称' });
+        if (name) {
+            const value = await vscode.window.showQuickPick(['仅自己可见（自己和知识库成员可见）', '互联网可见（互联网所有人可见）'], {
+                placeHolder: '请选择可见范围'
+            });
+            if(value) {
+                const publicVal = value === '互联网可见（互联网所有人可见）' ? 1 : 0;
+                try {
+                    await this.client.repos.create({ user: this.user.id, data: { name: name, slug: uuidv4(), type: "Book", public: publicVal } });
+                    vscode.window.showInformationMessage(msg);
+                    this.refresh();
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        }
     }
 
     refresh(): void {
